09e9c51176f3832beebe5c12d3a3099bcc01bb83
AlexKotty <Kot3Fallen88@gmail.com>
2022-03-19 16:31:07 +0300

    init changes

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ProgramCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int armour;
+            float health;
+            Console.WriteLine("Hello Stranger");
+
+
+
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
